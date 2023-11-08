7332079638b5f91895d657b3e7677b60d97a6198
Flávia Paixão <144342171+flapaixao@users.noreply.github.com>
2023-11-08 14:55:20 -0300

    Create teste.js

---
+++ Formação Lógica de Programação/Link dos Códigos/teste.js
@@ -0,0 +1,63 @@
+// Operdaores Aritiméticos
+
+let idade = 30
+console.log(idade)
+
+idade = 30 + 6
+console.log(idade)
+
+idade = 30 - 6
+console.log(idade)
+
+// concatenando com um texto
+idade = 30
+console.log("valor da minha variável " + idade)
+
+idade = 30 + 6
+console.log("operação de adição " + idade)
+
+idade = 30 - 6
+console.log("operação de subtração " + idade)
+
+// Fazendo o calculo com variáveis e números
+let codigoDoProduto = 1023
+let codigoExtao = codigoDoProduto - 1000
+console.log(codigoExtao)
+
+// Fazendo o calculo atribuindo variáveis... sem utilizar os numeros
+let primeiroNumero = 1023
+let segundoNumero = 23
+console.log(primeiroNumero - segundoNumero)
+
+let precoProduto = 100.99
+let valorComTaxo = precoProduto * 2
+console.log(valorComTaxo)
+
+let multiplicador = 4
+let multiplicando = 12
+let produto = multiplicador * multiplicando
+console.log("resultado da multiplicação é " + produto)
+
+// atribuindo novo valor para variável
+multiplicador = 8
+produto = multiplicador * multiplicando
+console.log("resultado da multiplicação é " + produto)
+
+let notadoMercado = 50
+let pessoasParaDividir = 2
+console.log("operação de divisão :" + notadoMercado / pessoasParaDividir)
+
+// Incremento e Decremento
+
+let contador = 1
+// "++" incremento
+contador++
+console.log(contador)
+
+// "--" decremento
+contador--
+contador--
+
+console.log(contador)
+
+// Calcular 
