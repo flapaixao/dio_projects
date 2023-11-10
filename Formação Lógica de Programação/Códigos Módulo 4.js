efb3175fdc0ef2b89854667fa57d23f3b12b1d19
Flávia Paixão <144342171+flapaixao@users.noreply.github.com>
2023-11-10 13:13:49 -0300

    Update Códigos Módulo 4.js

--- Formação Lógica de Programação/Códigos Módulo 4.js
+++ Formação Lógica de Programação/Códigos Módulo 4.js
@@ -46,3 +46,37 @@ function torrar(pao, nome, valor=99.00){
   console.log("O valor total é " + valor)
 }
 
+// FUNÇÕES DE RETORNO
+
+let resultado = soma(5, 5)
+
+function soma(numA, numB){
+  let somatorio = numA + numB
+  return somatorio
+}
+
+console.log("O Resultado dessa função " + resultado)
+
+// exemplo
+
+let userName = getFirstName("Flavia-Moraes", "-")
+console.log("Seja bem vindo " + userName)
+
+userName = getFirstName("Flavia Moraes", " ")
+console.log("Seja bem vindo " + userName)
+
+function getFirstName(name, splitChar){
+	let firstName = name.split(splitChar)[0]
+    return firstName
+}
+
+//
+
+function somar(numero1, numero2) {
+  return numero1 + numero2;
+}
+
+let resultado = somar(5, 3); // A função vai dar de presente 5 + 3 = 8
+console.log("A soma é: " + resultado); // Vai mostrar A soma é: 8
+
+
